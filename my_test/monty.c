7bfd7b76eced458cad76526eae25b426fa177e19
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-15 22:36:21 +0100

    Update monty.c

--- my_test/monty.c
+++ my_test/monty.c
@@ -2,7 +2,29 @@
 #include <stdlib.h>
 
 
-int main(int *argc, char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("First two Arguments are: %s and %s", *argv[0], *argv[1]);
+	FILE *file;
+	char *line_buff = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	file = fopen(argv[1], "r");
+	
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	
+	while((nread = getline(&line_buff, &len, file) != -1)
+	{
+		printf("%s\n", line_buff)
+	}
 }
