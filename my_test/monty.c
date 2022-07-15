4e8822e1472f358b3ed7d58aae8b001ae14e74a1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-15 22:59:10 +0100

    Update monty.c

--- my_test/monty.c
+++ my_test/monty.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[])
 	size_t len = 0;
 	ssize_t nread;
 	char *opcode;
-	char *arg;
 	
 	if (argc != 2)
 	{
@@ -29,8 +28,11 @@ int main(int argc, char *argv[])
 	while((nread = getline(&line_buff, &len, file)) != -1)
 	{
 		opcode = strtok(line_buff, "\n\t\r ");
-		printf("%s\n", opcode);
-		arg = strtok(NULL, "\n\t\r ");
-		printf("%i\n", atoi(arg));
+		if (!opcode && opcode[0] != '#')
+		{
+			printf("correct opcode");
+		}
+		
 	}
+	fclose(file);
 }
